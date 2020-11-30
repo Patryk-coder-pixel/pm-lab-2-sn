3da032e2e6151d5ddcc2d6db6e11969bced3a994
Patryk <patryk.lelakowski@student.wat.edu.pl>
2020-11-30 10:11:17 +0100

    Wykonano zadanie 2.2.3 – wskaźniki aktualizacja

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -4,10 +4,10 @@ uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
 uint8_t *pLedState = ledState;
 bool state = true;
 
-void togglePinD13(bool *state)
+void togglePinD13(bool state)
 {
-  PORTB = (*state << 5 );
-  *state = !(*state);
+  PORTB = (state << 5);
+  state = !(state);
 }
 
 void delay()
@@ -21,7 +21,7 @@ void delay()
 int main()
 {
   DDRB |= (1 << 5);
-  DDRB |= 0xFF;
+  DDRD |= 0xFF;
   while (1)
   {
     togglePinD13(&state);
